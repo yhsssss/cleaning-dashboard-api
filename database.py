416e8905850eccafc0b9808a55e5eb004e66e51d
yhsssss <yhsg2e@gmail.com>
2026-03-27 01:10:33 +0900

    테스트 커밋

--- database.py
+++ database.py
@@ -4,7 +4,6 @@ from sqlalchemy.orm import sessionmaker, Session
 from models import Base
 
 DB_URL = "postgresql+psycopg2://clinical_user:clinical_pass@localhost:5432/clinical_db"
-# 운영 전환 시: "postgresql+psycopg2://user:pass@host/dbname"
 
 engine = create_engine(
     DB_URL,
